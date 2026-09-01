4d0299c371630204d24cfa9a6e3325ba5aa0767b
abel <abeljo0207@gmail.com>
2026-09-01 12:53:30 +0530

    Added an @ in pass

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hospital_db',
         'USER': 'root',
-        'PASSWORD': 'admin123',
+        'PASSWORD': 'admin@123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
